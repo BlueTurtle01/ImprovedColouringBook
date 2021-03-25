e694537cd353e536b3a8849a4ea52cf02dcd3b9f
BlueTurtle01 <51229726+BlueTurtle01@users.noreply.github.com>
2021-03-25 08:06:27 +0000

    Updated commit

--- Contours.py
+++ Contours.py
@@ -31,17 +31,36 @@ def find_colour_pal(compressed_image):
 
     return palette
 
+def plot_histogram(imag):
+    plt.hist(imag.ravel(), 256) #Histogram is not bimodal so we can not use Otsu's thresholding.
+    plt.xlabel("Pixel Value")
+    plt.ylabel("Frequency")
+    plt.title("Histogram for input image")
+    plt.show()
+
+
+def plot_image(image, title, map="gray"):
+    plt.imshow(image, cmap=map)
+    plt.xticks([])
+    plt.yticks([])
+    plt.title(title)
+    plt.show()
+
 
 def kmeans(clusters):
-    file_name = "mia"
+    file_name = "paul"
     file_path = str("InputImages/" + file_name + ".jpg")
 
     #Read the image
-    imag = Image.open(file_path)
-    imshow(imag)
+    #imag = Image.open(file_path)
+    imag = cv2.imread(file_path)
+    plot_image(image=cv2.cvtColor(imag, cv2.COLOR_BGR2RGB), title="Original Image")
+    #plot_histogram(imag)
 
     #Dimension of the original image
-    cols, rows = imag.size
+    #cols, rows = imag.size
+    rows, cols, _ = imag.shape
+
 
     #Flatten the image
     imag = np.array(imag).reshape(rows*cols, 3)
@@ -68,7 +87,6 @@ def kmeans(clusters):
     return compressed_image, palette
 
 
-
 def auto_canny(image, sigma=0.5):
     # compute the median of the single channel pixel intensities
     v = np.median(image)
@@ -76,18 +94,43 @@ def auto_canny(image, sigma=0.5):
     lower = int(max(0, (1.0 - sigma) * v))
     upper = int(min(255, (1.0 + sigma) * v))
     edged = cv2.Canny(image, lower, upper)
-
     return edged
 
 
+def blend_outputs(outline):
+    alpha = 0.4 #This is the amount of the coloured image we include. 0 is none.
+    beta = 1 - alpha
+    compressed_image = imread("OutputImages/paul10Clustered.jpg")
+    compressed_image = cv2.cvtColor(compressed_image, cv2.COLOR_BGR2RGB)
+
+    outline = np.concatenate([outline[..., np.newaxis]] * 3, axis=2)
+    output = cv2.addWeighted(outline, beta, compressed_image, alpha, 0.0)
+
+    plt.imshow(output)
+    plt.title("Blended")
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
 def canny(img):
-    original_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    compressed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #imread reads images as Blue, Green, Red, Alpha
+    plot_image(image=cv2.cvtColor(img, cv2.COLOR_BGR2RGB), title="Compressed")
 
-    blurred = cv2.GaussianBlur(original_img, (15, 15), sigmaX=1)
+    k = 15
+    blurred = cv2.GaussianBlur(compressed_img, (k, k), sigmaX=(k-1)/6)
+    plot_image(image=blurred, title="Blurred", map="gray")
 
-    edged = auto_canny(blurred)
-    edged = cv2.dilate(edged, (5, 5), iterations=1)
-    edged = cv2.erode(edged, (5, 5), iterations=1)
+    th3 = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 3)
+    th3 = cv2.morphologyEx(th3, cv2.MORPH_CLOSE, (9, 9))
+
+    blend_outputs(th3)
+    plot_image(th3, title="Threshold Gray")
+
+    th3 = cv2.bitwise_not(th3)
+
+    edged = auto_canny(th3)
+    edged = cv2.dilate(edged, (20, 20), iterations=1)
+    edged = cv2.erode(edged, (20, 20), iterations=1)
 
     contours, hierarchy = cv2.findContours(edged, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
 
@@ -97,21 +140,15 @@ def canny(img):
 
     final_contours = []
     for contour in contours:
-        if cv2.contourArea(contour) > 50:
+        if cv2.contourArea(contour) > 40:
             final_contours.append(contour)
             pass
         else:
             pass
 
     temp_array = np.ones([rgb.shape[0], rgb.shape[1], rgb.shape[2]])
-    contours_ = cv2.drawContours(temp_array, final_contours, -1, (0, 0, 0), thickness=1)
-    plt.imshow(contours_)
-    plt.show()
-
-
-
-
-
+    contours_ = cv2.drawContours(temp_array, final_contours, -1, (0, 0, 0), thickness=3)
+    plot_image(contours_, title="Contours")
 
     # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
     # lines0 = cv2.drawContours(rgb, contours, 0, (0, 0, 0), 3)
@@ -144,5 +181,5 @@ def canny(img):
     return rgb
 
 
-img, palette = kmeans(int(5))
+img, palette = kmeans(int(10))
 canny(img)

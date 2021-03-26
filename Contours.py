f8c3d1ff27a1c31e38d70f9976414922987f662e
BlueTurtle01 <51229726+BlueTurtle01@users.noreply.github.com>
2021-03-26 10:25:33 +0000

    Concat 2 k-means functions into 1

--- Contours.py
+++ Contours.py
@@ -7,8 +7,18 @@ from sklearn.cluster import KMeans
 import pandas as pd
 import os
 
+file_name = "astrocute"
+file_path = str("InputImages/" + file_name + ".jpg")
+
 
 def find_colour_pal(compressed_image, im_name):
+    """
+    This function draws a grid of the colours used for the clusters. This gives the user an easy way to check they have the correct colouring crayons.
+
+    :param compressed_image: Numpy array of the pixel values for the compressed image after applying k-means to the original imput image.
+    :param im_name: Name of the file to be used as the filename when saving.
+    :return: A palette of the cluster colours
+    """
     # Credit: https://stackoverflow.com/a/51729498/4367851
     temp_image = Image.fromarray(np.uint8(compressed_image)).convert('RGB')
     colours = Image.Image.getcolors(temp_image)
@@ -21,7 +31,12 @@ def find_colour_pal(compressed_image, im_name):
 
     RGB = df["RGB"].tolist()
     palette = np.array(RGB)[np.newaxis, :, :]
-    palette = palette.reshape(2, -1, 3)
+
+    if palette.shape[0] % 2 == 0:
+        palette = palette.reshape(2, -1, 3)
+    elif palette.shape[0] % 3 == 0:
+        palette = palette.reshape(3, -1, 3)
+
     plt.figure(figsize=(20, 10))
     plt.imshow(palette)
     plt.xticks([])
@@ -33,7 +48,7 @@ def find_colour_pal(compressed_image, im_name):
 
 
 def plot_histogram(imag):
-    plt.hist(imag.ravel(), 256) #Histogram is not bimodal so we can not use Otsu's thresholding.
+    plt.hist(imag.ravel()/255., 256)  # Histogram is not bimodal so we can not use Otsu's thresholding.
     plt.xlabel("Pixel Value")
     plt.ylabel("Frequency")
     plt.title("Histogram for input image")
@@ -50,23 +65,18 @@ def plot_image(image, title, im_name, map="gray"):
 
 
 def kmeans(clusters):
-    file_name = "astrocute"
-    file_path = str("InputImages/" + file_name + ".jpg")
-
-    #Read the image
-    #imag = Image.open(file_path)
+    # Read the image
     imag = cv2.imread(file_path)
     plot_image(image=cv2.cvtColor(imag, cv2.COLOR_BGR2RGB), title="Original Image", im_name=file_name)
-    #plot_histogram(imag)
+    plot_histogram(imag)
 
-    #Dimension of the original image
-    #cols, rows = imag.size
+    # Dimension of the original image
     rows, cols, _ = imag.shape
 
-    #Flatten the image
+    # Flatten the image
     imag = np.array(imag).reshape(rows*cols, 3)
 
-    #Implement k-means clustering to form k clusters
+    # Implement k-means clustering to form k clusters
     kmeans = KMeans(n_clusters=clusters)
     kmeans.fit(imag)
 
@@ -78,15 +88,14 @@ def kmeans(clusters):
     compressed_image = compressed_image.reshape(rows, cols, 3)
     palette = find_colour_pal(compressed_image, im_name=file_name)
 
-
     # Save and display output image
-    if not os.path.isdir("OutputImages/" + str(file_name)):
-        os.mkdir("OutputImages/" + str(file_name))
-    else:
+    if os.path.isdir("OutputImages/" + str(file_name)):
         pass
+    else:
+        os.mkdir("OutputImages/" + str(file_name))
     imsave(("OutputImages/" + str(file_name) + "/" + str(file_name) + str(clusters) + "Clustered.jpg"), compressed_image)
 
-    return compressed_image, palette, file_name
+    return compressed_image, palette
 
 
 def auto_canny(image, sigma=0.5):
@@ -99,7 +108,15 @@ def auto_canny(image, sigma=0.5):
     return edged
 
 
-def blend_outputs(outline, file_name, clusters):
+def blend_outputs(outline, clusters):
+    """
+    Some of the edges are not closed loops so it can be confusing where one colour stops and another starts.
+    I think overlaying the outlines onto a slightly transparent version of the compressed image gives a good guide to the user for which colour to fill each area with.
+
+    :param outline: The numpy array of outline pixel values
+    :param clusters: Number of clusters from the k-means algorithm
+    :return: NA
+    """
     alpha = 0.4  # This is the amount of the coloured image we include. 0 is none.
     beta = 1 - alpha
     compressed_image = imread(("OutputImages/" + str(file_name) + "/" + str(file_name) + str(clusters) + "Clustered.jpg"))
@@ -115,26 +132,14 @@ def blend_outputs(outline, file_name, clusters):
     plt.show()
 
 
-def canny(img, file_name):
-    compressed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #imread reads images as Blue, Green, Red, Alpha
-    plot_image(image=cv2.cvtColor(img, cv2.COLOR_BGR2RGB), title="Compressed", im_name=file_name)
-
-    k = 15
-    blurred = cv2.GaussianBlur(compressed_img, (k, k), sigmaX=(k-1)/6)
-    plot_image(image=blurred, title="Blurred", im_name=file_name, map="gray")
-
-    th3 = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 3)
-    th3 = cv2.morphologyEx(th3, cv2.MORPH_CLOSE, (9, 9))
-
-    blend_outputs(th3, file_name, clusters=20)
-    plot_image(th3, title="Threshold Gray", im_name=file_name)
-
-    th3 = cv2.bitwise_not(th3)
-
-    edged = auto_canny(th3)
-    edged = cv2.dilate(edged, (20, 20), iterations=1)
-    edged = cv2.erode(edged, (20, 20), iterations=1)
-
+def draw_contours(edged):
+    """
+    Draw the contours over a blank array. The function cv2.DrawContours overlays the contours on top of the bitwise array.
+    Which is not ideal if the bitwise array contains some small, noisy contours. Therefore, I created an empty array first and then used this as the base
+    for drawing the contours onto.
+    :param edged: Output of the Canny Edge Detection algorithm after applying erode and dilation.
+    :return:
+    """
     contours, hierarchy = cv2.findContours(edged, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
 
     edged = cv2.bitwise_not(edged)
@@ -151,7 +156,41 @@ def canny(img, file_name):
 
     temp_array = np.ones([rgb.shape[0], rgb.shape[1], rgb.shape[2]])
     contours_ = cv2.drawContours(temp_array, final_contours, -1, (0, 0, 0), thickness=3)
+
     plot_image(contours_, title="Contours", im_name=file_name)
+
+
+def main_func(img, clusters):
+    """
+
+    :param img:
+    :param clusters: Number of clusters
+    :return: NA
+    """
+
+    # The output of the k-means algorithm is an array in the form of Blue,Green,Red (BGR) which needs to be converted to grayscale before we conduct a Gaussian Blur.
+    compressed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # imread reads images as Blue, Green, Red, Alpha
+    plot_image(image=cv2.cvtColor(img, cv2.COLOR_BGR2RGB), title="Compressed", im_name=file_name)
+
+    k = 15  # Size of the kernal for the Gaussian Blur
+    blurred = cv2.GaussianBlur(compressed_img, (k, k), sigmaX=(k-1)/6)
+    plot_image(image=blurred, title="Blurred", im_name=file_name, map="gray")
+
+    # Conduct Adaptive Thresholding to the blurred image
+    threshold_image = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 3)
+    threshold_image = cv2.morphologyEx(threshold_image, cv2.MORPH_CLOSE, (9, 9))
+
+    # Overlay the threshold_image on top of a slightly transparent copy of the clustered image.
+    blend_outputs(threshold_image, clusters=clusters)
+    plot_image(threshold_image, title="Threshold Gray", im_name=file_name)
+
+    inverted_threshold = cv2.bitwise_not(threshold_image)
+
+    edged = auto_canny(threshold_image)
+    edged = cv2.dilate(edged, (20, 20), iterations=1)
+    edged = cv2.erode(edged, (20, 20), iterations=1)
+
+    draw_contours(edged)
 
     # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
     # lines0 = cv2.drawContours(rgb, contours, 0, (0, 0, 0), 3)
@@ -181,13 +220,13 @@ def canny(img, file_name):
     # plt.savefig("ContoursOutput.jpg", dpi = 400, bbox_inches='tight')
     # plt.show()
 
-    return rgb
 
 def calculate_clusters():
     # Credit: https://blog.cambridgespark.com/how-to-determine-the-optimal-number-of-clusters-for-k-means-clustering-14f27070048f
-    file_name = "astrocute"
-    file_path = str("InputImages/" + str(file_name) + ".jpg")
     imag = cv2.imread(file_path)
+    plot_image(image=cv2.cvtColor(imag, cv2.COLOR_BGR2RGB), title="Original Image", im_name=file_name)
+    plot_histogram(imag)
+
     Sum_of_squared_distances = []
     # Dimension of the original image
     rows, cols, _ = imag.shape
@@ -195,21 +234,54 @@ def calculate_clusters():
     # Flatten the image
     imag = np.array(imag).reshape(rows*cols, 3)
 
-    K = range(1, 20)
+    K = range(1, 5)
     for k in K:
         km = KMeans(n_clusters=k)
         km.fit(imag)
         Sum_of_squared_distances.append(km.inertia_)
 
+    error_df = pd.DataFrame(list(zip(K, Sum_of_squared_distances)), columns=["K", "Error"])
+    error_df["Difference"] = error_df["Error"] - error_df["Error"].shift(-1)
+
     plt.plot(K, Sum_of_squared_distances, 'bx-')
+    plt.plot(K, error_df["Difference"])
+    #plt.yscale('log')
     plt.xlabel('k')
     plt.ylabel('Sum of Squared Distances')
     plt.title('Elbow Method For Optimal k')
     plt.show()
 
-#calculate_clusters()
+    try:
+        clusters = min(error_df.index[error_df["Difference"] < 3e+06].tolist())
+    except ValueError:
+        clusters = max(error_df["K"])
 
+    # Implement k-means clustering to form k clusters
+    kmeans = KMeans(n_clusters=clusters)
+    kmeans.fit(imag)
 
-img, palette, file_name = kmeans(int(8))
-canny(img, file_name)
+    #  Replace each pixel value with its nearby centroid
+    compressed_image = kmeans.cluster_centers_[kmeans.labels_]
+    compressed_image = np.clip(compressed_image.astype('uint8'), 0, 255)
+
+    #  Reshape the image to original dimension
+    compressed_image = compressed_image.reshape(rows, cols, 3)
+    find_colour_pal(compressed_image, im_name=file_name)
+
+    # Save and display output image
+    if os.path.isdir("OutputImages/" + str(file_name)):
+        pass
+    else:
+        os.mkdir("OutputImages/" + str(file_name))
+    imsave(("OutputImages/" + str(file_name) + "/" + str(file_name) + str(clusters) + "Clustered.jpg"), compressed_image)
+
+    try:
+        return min(error_df.index[error_df["Difference"] < 3e+06].tolist())
+    except ValueError:
+        return max(error_df["K"])
+
+clusters = calculate_clusters()
+
+img, palette = kmeans(int(clusters))
+main_func(img, clusters=clusters)
 
